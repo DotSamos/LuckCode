4d7a02764c39cb1512e6535e880d394b775089b2
SamosMC <joaomarcoslima810@gmail.com>
2021-01-03 16:27:59 -0200

    Criada a interface e o Gerenciador de datas

---
+++ src/luckCode/data/manager/IDataManager.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace luckCode\data\manager;
+
+use luckCode\data\interfaces\IData;
+
+interface IDataManager
+{
+
+    public function loadDefaults();
+
+    /**
+     * @param string $file
+     * @return bool
+     */
+    public function contains(string $file) : bool;
+
+    /**
+     * @param string $file
+     * @return IData|null
+     */
+    public function get(string $file);
+
+    /**
+     * @param IData $data
+     * @return bool
+     */
+    public function put(IData $data) : bool;
+
+    /**
+     * @param string $file
+     * @return bool
+     */
+    public function remove(string $file) : bool;
+
+    public function putAllInSaveWorker();
+
+    public function forceSaveAll();
+}
