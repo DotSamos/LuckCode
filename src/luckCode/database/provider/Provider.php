2fb2d16d0b4cf8af07e135c4694ad29cfeb6e156
SamosMC <joaomarcoslima810@gmail.com>
2021-01-05 11:36:30 -0200

    Criada a abstração dos provedores de dados (database)

---
+++ src/luckCode/database/provider/Provider.php
@@ -0,0 +1,94 @@
+<?php
+
+declare(strict_types=1);
+
+namespace luckCode\database\provider;
+
+use luckCode\utils\InfoStatus;
+use pocketmine\plugin\PluginBase;
+use pocketmine\plugin\PluginLogger;
+use Throwable;
+
+abstract class Provider implements InfoStatus, interfaces\IProvider
+{
+
+    /** @var PluginBase $ownerPlugin */
+    protected $ownerPlugin;
+
+    /** @var Throwable $lastError */
+    protected $lastError;
+
+    protected $connection;
+
+    /**
+     * Provider constructor.
+     * @param array $conArgs
+     * @param PluginBase $owner
+     */
+    public function __construct(array $conArgs, PluginBase $owner)
+    {
+        $this->ownerPlugin = $owner;
+        $this->tryConnect($conArgs);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getOwnerPlugin(): PluginBase
+    {
+        return $this->ownerPlugin;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getLogger(): PluginLogger
+    {
+        return $this->ownerPlugin->getLogger();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function showInfo(string $info)
+    {
+        $this->getLogger()->info(self::INFO_PREFIX.'§b[Provider '.$this->getType().'] §7'.$info);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function showAlert(string $alert)
+    {
+        $this->getLogger()->info(self::ALERT_PREFIX.'§b[Provider '.$this->getType().'] §7'.$alert);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function showError(string $error)
+    {
+        $this->getLogger()->info(self::ERROR_PREFIX.'§b[Provider '.$this->getType().'] §7'.$error);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function printError(Throwable $error)
+    {
+        $this->showError(implode("§r\n", [
+            '§4('.$error->getCode().') §7'.$error->getMessage(),
+            '§7Em §f'.$error->getFile().' ['.$error->getLine().']'
+        ]));
+    }
+
+    public function getRawConnection()
+    {
+        return $this->connection;
+    }
+
+    public function fail()
+    {
+        return $this->lastError;
+    }
+}
---
+++ src/luckCode/database/provider/interfaces/IProvider.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace luckCode\database\provider\interfaces;
+
+use Exception;
+use pocketmine\plugin\PluginBase;
+
+interface IProvider
+{
+
+    /** @return PluginBase */
+    public function getOwnerPlugin() : PluginBase;
+
+    public function getRawConnection();
+
+    /** @return string */
+    public function getType() : string;
+
+    /** @return Exception|null */
+    public function fail();
+
+    /**
+     * @param array $args
+     * @return bool
+     */
+    public function tryConnect(array $args) : bool;
+
+    /** @return bool */
+    public function close() : bool;
+
+    /**
+     * @param string $exec
+     * @return bool
+     */
+    public function exec(string $exec) : bool;
+
+    /**
+     * @param string $query
+     * @param bool $fetchAll
+     * @return array
+     */
+    public function executeQuery(string $query, $fetchAll = false) : array;
+}
