17ca38ba636afcb1a127e58ea4bf80e9ee7177aa
SamosMC <joaomarcoslima810@gmail.com>
2021-01-05 11:35:48 -0200

    Removida use desnecessáia
    A use "luckCode\data\types\YamlData" foi removida

--- src/luckCode/LuckCodePlugin.php
+++ src/luckCode/LuckCodePlugin.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace luckCode;
 
 use luckCode\data\save\manager\DataSaveWorker;
-use luckCode\data\types\YamlData;
 use pocketmine\plugin\PluginBase;
 use pocketmine\Server;
 
