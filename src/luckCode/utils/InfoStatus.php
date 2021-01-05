904e3db294e5184e643a2ed643b247f6c676d9bc
SamosMC <joaomarcoslima810@gmail.com>
2021-01-05 11:38:45 -0200

    Criada a interface para sistemas
    (Apenas para mandar mensagens no terminal de uma forma mais "organizada")

---
+++ src/luckCode/utils/InfoStatus.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace luckCode\utils;
+
+use pocketmine\plugin\PluginLogger;
+use Throwable;
+
+interface InfoStatus
+{
+
+    const INFO_PREFIX = '§7[Info]';
+    const ALERT_PREFIX = '§e[Alerta]';
+    const ERROR_PREFIX = '§c[Erro]';
+
+    /** @return PluginLogger */
+    public function getLogger() : PluginLogger;
+
+    /** @param string $info */
+    public function showInfo(string $info);
+
+    /** @param string $alert */
+    public function showAlert(string $alert);
+
+    /** @param string $error */
+    public function showError(string $error);
+
+    /** @param Throwable $error */
+    public function printError(Throwable $error);
+}
