2564d094f1782b861895743d280136dbde92878b
SamosMC <joaomarcoslima810@gmail.com>
2021-01-05 11:37:57 -0200

    Criada o tipo de provedor PDO (mysqli e sqlite3)
    O sistema foi previamente testado criando tabelas, inserindo valores e executando querys. Ainda podem haver erros ou bugs
    (Provavelmente não é toda a hospedagem que vai dar suporte para utilzar o PDO)

---
+++ src/luckCode/database/provider/PDOProvider.php
@@ -0,0 +1,86 @@
+<?php
+
+declare(strict_types=1);
+
+namespace luckCode\database\provider;
+
+use PDO;
+use PDOException;
+
+abstract class PDOProvider extends Provider
+{
+
+    /** @var PDO $connection */
+    protected $connection;
+
+    /**
+     * @param array $args
+     * @return string
+     */
+    public abstract function getDSN(array $args) : string;
+
+    /**
+     * @return array
+     */
+    public abstract function getDriverOptions() : array;
+
+    /**
+     * @inheritDoc
+     */
+    public function tryConnect(array $args): bool
+    {
+        try {
+            $pdo = new PDO($this->getDSN($args),
+                $args['user'] ?? '',
+                $args['password'] ?? '',
+                $this->getDriverOptions()
+            );
+            $this->connection = $pdo;
+            $this->showInfo('§aOK.');
+            return true;
+        } catch (PDOException $e) {
+            $this->lastError = $e;
+            $this->showError('§cFalha na conexão!');
+            $this->printError($e);
+            return false;
+        }
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function close(): bool
+    {
+        $this->connection = null; // sim, nem eu acreditei que é assim que fecha a conexão com o PDO '-'
+        return true;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function exec(string $exec): bool
+    {
+        try {
+            return $this->connection->exec($exec) === false;
+        } catch (PDOException $e) {
+            $this->lastError = $e;
+            $this->printError($e);
+            return false;
+        }
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function executeQuery(string $query, $fetchAll = false): array
+    {
+        try {
+            $result = $this->connection->query($query);
+            return $fetchAll ? $result->fetchAll() : $result->fetch();
+        } catch (PDOException $e) {
+            $this->lastError = $e;
+            $this->printError($e);
+            return [];
+        }
+    }
+}
