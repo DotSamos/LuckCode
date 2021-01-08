5872b532e38b127fcbe0de04333c6ac90c37fe05
SamosMC <joaomarcoslima810@gmail.com>
2021-01-08 12:31:08 -0200

    Criada a abstração da database (e suporte as tabelas)

---
+++ src/luckCode/database/interfaces/IDatabase.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace luckCode\database\interfaces;
+
+use luckCode\database\provider\interfaces\IProvider;
+use luckCode\database\table\interfaces\ITable;
+use pocketmine\plugin\PluginBase;
+
+interface IDatabase
+{
+
+    /** @return IProvider */
+    public function getProvider() : IProvider;
+
+    /** @return PluginBase */
+    public function getOwnerPlugin() : PluginBase;
+
+    public function onPreLoadTables();
+
+    public function onInvalidProvider();
+
+    /**
+     * @param ITable $table
+     * @return bool
+     */
+    public function addTable(ITable $table) : bool;
+
+    /**
+     * @param string $name
+     * @return ITable|null
+     */
+    public function getTable(string $name);
+
+    /** @return string[] */
+    public function getDefaultTables() : array;
+
+    /**
+     * @return bool
+     */
+    public function loadTables() : bool;
+}
