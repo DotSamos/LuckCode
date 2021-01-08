bd735a36751aa77aa15a2126780d15ad088069c8
SamosMC <joaomarcoslima810@gmail.com>
2021-01-08 13:09:29 -0200

    Criada a task de inicialização do banco de dados

---
+++ src/luckCode/scheduler/loader/DatabaseLoaderWaitTask.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace luckCode\scheduler\loader;
+
+use luckCode\LuckCodePlugin;
+use luckCode\scheduler\LuckTask;
+use pocketmine\plugin\PluginBase;
+use function array_filter;
+use function count;
+
+class DatabaseLoaderWaitTask extends LuckTask
+{
+    /**
+     * @inheritDoc
+     */
+    public function onRun($currentTick)
+    {
+        $pl = LuckCodePlugin::getInstance();
+        $wait = array_filter($pl->getServer()->getPluginManager()->getPlugins(), function (PluginBase $check){
+           return !$check->isEnabled();
+        });
+        if(!count($wait)) {
+            $pl->loadDatabase();
+            $this->cancel();
+        }
+    }
+}
