573fb09744798c839b3f104de43308fde55e584d
SamosMC <joaomarcoslima810@gmail.com>
2021-01-09 22:12:12 -0200

    Criado a abstração dos hologramas + holograma de teste

---
+++ src/luckCode/entity/holographic/LuckHolographicEntity.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace luckCode\entity\holographic;
+
+use luckCode\LuckCodePlugin;
+use pocketmine\Player;
+use function array_walk;
+use function str_repeat;
+use function strlen;
+use function substr;
+
+class LuckHolographicEntity extends LuckHolographicEntityBase
+{
+
+    /** @var int $typeBar */
+    private $nextColor = 0;
+
+    public function onUpdate($tick)
+    {
+        $bar = str_repeat('-', 12);
+        if($this->nextColor++ == strlen($bar)) {
+            $this->nextColor = 0;
+        }
+        $barFront = substr($bar, $this->nextColor);
+        $barRedo = substr($bar, strlen($bar) - $this->nextColor);
+        $bar = '§f'.$barFront.'§b--§f'.$barRedo;
+        $text = $bar."\n§r§l§3 Luck§5Code§r §7v".LuckCodePlugin::VERSION."\n".$bar;
+
+        array_walk($this->hasSpawned, function (Player $p) use($text) {
+           $this->setNameTagFor($p, $text);
+        });
+
+        return parent::onUpdate($tick);
+    }
+}
