cf5e017e7dfe6b015a1688cff924c899315100fe
SamosMC <joaomarcoslima810@gmail.com>
2021-01-13 09:12:01 -0200

    Criada a abstração das paginas do menu

---
+++ src/luckCode/menu/page/interfaces/IPaginatedMenu.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace luckCode\menu\page\interfaces;
+
+use pocketmine\item\Item;
+
+interface IPaginatedMenu
+{
+
+    /** @param IPage $page */
+    public function setPage(IPage $page);
+
+    /** @param Item[] $items */
+    public function setItems(array $items);
+
+    /** @return IPage */
+    public function getMainPage() : IPage;
+
+    public function redoPage();
+}
