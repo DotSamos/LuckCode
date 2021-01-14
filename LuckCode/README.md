5e070fe071c4da066455c680388b7bcc1eba4ca1
SmallkingDev <57811430+SmallkingDev@users.noreply.github.com>
2021-01-14 15:48:48 +0000

    Delete LuckCode directory

--- LuckCode/README.md
+++
@@ -1,4 +0,0 @@
-# LuckCode
-:hammer: Uma "caixa de ferramentas" para desenvolver plugins para Pocketmine-MP (v15.10)
-
-:calendar: Iniciado no dia 31/12/2020 14:04
--- LuckCode/src/luckCode/database/table/ProfileTable.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace luckCode\database\table;
-
-abstract class ProfileTable extends Table implements interfaces\IProfileTable
-{}
