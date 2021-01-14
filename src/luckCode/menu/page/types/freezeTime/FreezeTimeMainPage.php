27fb7679529b829e29f9542ec573d8d33ad112c1
SamosMC <joaomarcoslima810@gmail.com>
2021-01-14 12:03:29 -0200

    Criada a página principal do menu do freeze-time

---
+++ src/luckCode/menu/page/types/freezeTime/FreezeTimeMainPage.php
@@ -0,0 +1,89 @@
+<?php
+
+declare(strict_types=1);
+
+namespace luckCode\menu\page\types\freezeTime;
+
+use luckCode\LuckCodePlugin;
+use luckCode\menu\page\Page;
+use luckCode\system\types\FreezeTimeSystem;
+use pocketmine\block\Block;
+use pocketmine\block\Wool;
+use pocketmine\item\Dye;
+use pocketmine\item\Item;
+use pocketmine\level\Level;
+use pocketmine\Player;
+
+class FreezeTimeMainPage extends Page
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function getItems(Player $player): array
+    {
+        $day = Item::get(Block::WOOL, Wool::CYAN)->setCustomName('§r§bDia');
+        $midday = Item::get(Block::WOOL, Wool::YELLOW)->setCustomName('§r§6Meio-dia');
+        $night = Item::get(Block::WOOL, Wool::BLACK)->setCustomName('§r§7Noite');
+        $unFreeze = Item::get(Item::CLOCK)->setCustomName('§r§aDestravar tempo no mundo');
+
+        $timeWord = FreezeTimeSystem::$worlds[$player->level->getName()] ?? null;
+
+        $addUnFreeze = false;
+        $slotTime = null;
+        if($timeWord === Level::TIME_DAY) {
+            $addUnFreeze = true;
+            $slotTime = 2;
+        } else if($timeWord === 6000) {
+            $addUnFreeze = true;
+            $slotTime = 3;
+        } else if($timeWord === Level::TIME_NIGHT) {
+            $addUnFreeze = true;
+            $slotTime = 4;
+        }
+
+        $items = [
+            10 => $day,
+            11 => $midday,
+            12 => $night
+        ];
+
+        for($i = 2; $i <= 4; $i++) {
+            $items[$i] = Item::get(Item::DYE, ($i == $slotTime ? Dye::LIME : Dye::GRAY))->setCustomName("§8");
+        }
+
+        if($addUnFreeze) {
+            $items[23] = $unFreeze;
+        }
+        return $items;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function onClick(Player $player, Item $item): bool
+    {
+        $prefix = LuckCodePlugin::PREFIX;
+        $level = $player->getLevel()->getName();
+        $name = $item->getCustomName();
+
+        $time = null;
+        if($name == '§r§bDia') {
+            $time = Level::TIME_DAY;
+        } else if($name == '§r§6Meio-dia') {
+            $time = 6000;
+        } else if($name == '§r§7Noite') {
+            $time = Level::TIME_NIGHT;
+        } else if($name == '§r§aDestravar tempo no mundo') {
+            unset(FreezeTimeSystem::$worlds[$level]);
+            $player->removeWindow($this->menu);
+            $player->sendMessage($prefix.'§aTempo no mundo destravado!');
+            return true;
+        }
+        if($time != null) {
+            FreezeTimeSystem::$worlds[$level] = $time;
+            $this->menu->setPage($this); // atualizar itens
+        }
+        return true;
+    }
+}
