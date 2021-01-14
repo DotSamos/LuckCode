d2cb1152a41e7cb1df2ed0e13cda415bd93cf487
SamosMC <joaomarcoslima810@gmail.com>
2021-01-14 12:00:33 -0200

    Adicionado os novos sistemas e criado o getter da database
    Adicionado o system-loader e o getter da database

--- src/luckCode/LuckCodePlugin.php
+++ src/luckCode/LuckCodePlugin.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace luckCode;
 
-use function implode;
+use luckCode\plugin\interfaces\LuckSystemLoader;
+use luckCode\scheduler\LuckUtilityTask;
+use luckCode\system\controller\SystemController;
 use luckCode\command\defaults\LuckCodeCommand;
 use luckCode\data\manager\types\LuckDataManager;
 use luckCode\data\save\manager\DataSaveWorker;
@@ -13,18 +15,26 @@ use luckCode\entity\EntityManager;
 use luckCode\menu\manager\MenuController;
 use luckCode\menu\tile\MenuChestTile;
 use luckCode\scheduler\loader\DatabaseLoaderWaitTask;
+use luckCode\system\types\FreezeTimeSystem;
+use luckCode\system\types\LuckCommandSystem;
+use luckCode\system\types\LuckDatabaseSystem;
 use luckCode\utils\ProviderLoader;
 use luckCode\utils\text\TextFormatter;
 use pocketmine\Server;
 use pocketmine\plugin\PluginBase;
 use pocketmine\tile\Tile;
 
-class LuckCodePlugin extends PluginBase
+use function implode;
+
+class LuckCodePlugin extends PluginBase implements LuckSystemLoader
 {
 
     const PREFIX = '§f[§l§3L§5C§r§f] ';
     const VERSION = 0.1;
     const ADMIN_PERMISSION = 'luckcode.admin';
+
+    /** @var SystemController $systemController */
+    private $systemController;
 
     /** @var LuckDataManager $dataManager */
     private $dataManager;
@@ -44,16 +54,15 @@ class LuckCodePlugin extends PluginBase
     public function onLoad()
     {
         self::$instance = $this;
+        $this->dataManager = new LuckDataManager();
+        $this->systemController = ($sc = new SystemController($this));
+        $sc->onLoad();
     }
 
     public function onEnable()
     {
-        $this->dataManager = ($dataManager = $this->loadDataManager());
-        (new DatabaseLoaderWaitTask())->registerToRepeat(1);
-        EntityManager::registerDefaults();
-        (new LuckCodeCommand())->registerCommand($this, 'samos.luckcode.command');
-        Tile::registerTile(MenuChestTile::class);
-
+        $this->loadBase();
+        $this->systemController->onEnable();
         Server::getInstance()->getLogger()->info(TextFormatter::center(implode("§r\n", [
             '§8',
             '§8',
@@ -67,10 +76,9 @@ class LuckCodePlugin extends PluginBase
 
     public function onDisable()
     {
+        $this->systemController->onDisable();
         MenuController::closeAll('§cMenu fechado devido a um evento inesperado. Tente reabrir ele novamente dentro de alguns segundos...');
         DataSaveWorker::startWorker();
-        $database = $this->database;
-        if ($database) $database->close();
     }
 
     /** @return LuckDataManager */
@@ -79,9 +87,15 @@ class LuckCodePlugin extends PluginBase
         return $this->dataManager;
     }
 
-    private function loadDataManager(): LuckDataManager
-    {
-        return $this->dataManager = new LuckDataManager();
+    /** @return LuckDatabase|null */
+    public function getDatabase() {
+        return $this->database;
+    }
+
+    private function loadBase() {
+        EntityManager::registerDefaults();
+        Tile::registerTile(MenuChestTile::class);
+        (new LuckUtilityTask())->registerToRepeat();
     }
 
     public function loadDatabase()
@@ -89,12 +103,39 @@ class LuckCodePlugin extends PluginBase
         if (!$this->database) {
             $providerData = $this->dataManager->get('database')->getContents();
             $provider = (new ProviderLoader($this, $providerData['type_priority'], $providerData['mysqli_auth']))->get();
+            $this->database = ($database = new LuckDatabase($provider, $this));
             if ($provider == null) {
                 $this->getLogger()->info('§cNenhum provedor de dados pode ser inicializado!');
                 $this->getServer()->getPluginManager()->disablePlugin($this);
-            } else {
-                $this->database = new LuckDatabase($provider, $this);
             }
         }
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getSystemStatusList(): array
+    {
+        return $this->dataManager->get('systems')->getContents();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getSystemsBases(): array
+    {
+        return [
+            LuckDatabaseSystem::class,
+            LuckCommandSystem::class,
+            FreezeTimeSystem::class
+        ];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getSystemController(): SystemController
+    {
+        return $this->systemController;
     }
 }
