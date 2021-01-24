35ec2b14d0a75228ca93e380bba8da56aaf6b416
SamosMC <joaomarcoslima810@gmail.com>
2021-01-24 12:36:16 -0200

    Edição na estetica

--- src/luckCode/utils/Utils.php
+++ src/luckCode/utils/Utils.php
@@ -21,10 +21,12 @@ final class Utils {
     public static function getThrowablePrint(Throwable $error) : string {
         $pos = strpos($error->getFile(), 'luckCode');
         return implode("\n", [
-            "§8",
+            '§8',
+            '§c|',
             '§c| §4(' . $error->getCode() . ') §7' . wordwrap(str_replace("\n", '', $error->getMessage()), 80, "\n§c|§7 "),
             "§c| §7Line §f{$error->getLine()}§7 from:",
-            "§c| §b" . wordwrap(is_numeric($pos) ? substr($error->getFile(), $pos) : $error->getFile(), 80, "\n§c|§7 ")
+            "§c| §b" . wordwrap(is_numeric($pos) ? substr($error->getFile(), $pos) : $error->getFile(), 80, "\n§c|§7 "),
+            '§c|'
         ]);
     }
 }
